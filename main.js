ea6f112b455466cb0b378cb2b1beb50030349ed7
Riham Zeidan <64324442+sudo-riham@users.noreply.github.com>
2023-01-06 14:34:48 +0200

    Completed Assignment

--- main.js
+++ main.js
@@ -2,11 +2,30 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const liker = document.querySelector(".like-glyph")
 
-
-
-
+liker.addEventListener("click", function(e){
+  mimicServerCall()
+  .then(function(response){
+    console.log(response)
+    console.log(e.target.innerText);
+    if(e.target.innerText === EMPTY_HEART){
+      e.target.innerText = FULL_HEART
+      e.target.classList.add("activated-heart")
+    } else {
+      e.target.innerText = EMPTY_HEART
+      e.target.classList.remove("activated-heart")
+    }
+  })
+  .catch(function(error){
+    const modal = document.querySelector("#modal")
+    modal.classList.remove("hidden")
+    modal.innerText = error
+    setTimeout(function(){
+      modal.classList.add("hidden")
+    }, 5000)
+  })
+});
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
